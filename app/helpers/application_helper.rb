368dad091d8bd52495f9cc776f494c65e686a5d8
Joan Wolkerstorfer <joanwolk@gmail.com>
2011-08-28 01:06:53 +0200

    Add realm status to sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
+require 'net/http'
 module ApplicationHelper
   
   # Return a title on a per-page basis.
@@ -7,6 +8,19 @@ module ApplicationHelper
       base_title
     else
       "#{base_title} | #{@title}"
+    end
+  end
+
+  def uldaman_status
+    http = Net::HTTP.new("us.battle.net", 80)
+    realm_status = http.request(Net::HTTP::Get.new("/api/wow/realm/status?realms=Uldaman"))
+    @uldaman_status = JSON.parse(realm_status.body)['realms'][0]['status']
+    if @uldaman_status == true
+      "up"
+    elsif @uldaman_status == false
+      "down"
+    else
+      "status check broken"
     end
   end
 
